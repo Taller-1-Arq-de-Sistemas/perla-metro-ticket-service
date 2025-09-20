a6f03dbe03227df4bd61de83732f79d66dec9fd4
Thetrolxs <moralesharnisch@gmail.com>
2025-09-19 21:02:02 -0300

    feat: add ticket model

---
+++ perla-metro-tickets-service/src/models/Ticket.cs
@@ -0,0 +1,43 @@
+using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace perla_metro_tickets_service.src.models
+{
+    public enum TicketType {one_way, round_trip}
+    public enum TicketState { Active, Used, Expired, Inactive}
+    public class Ticket
+    {
+        [BsonId]
+        [BsonRepresentation(BsonType.String)]
+        public Guid Id { get; set; } = Guid.NewGuid();
+
+        public Guid PassagerId { get; set; }
+
+        //Fecha y hora de emición del ticket
+        public DateTime IssuedAt { get; set; } = DateTime.UtcNow;
+        public DateTime IssuedDate { get; set; } //Fecha y hora de emisión normalizada
+
+        //Tipo de ticket
+        public TicketType Type { get; set; }
+        //Estado del ticket
+        public TicketState State { get; set; } = TicketState.Active;
+        //Monto pagado por el ticket
+        public decimal Amount { get; set; }
+        //eliminación (soft Delete) del ticket
+        public bool IsDeleted { get; set; } = false; //False = Activo, True = Borrado
+
+        //ATRIBUTOS DE AUDITORIA
+        //Fecha de eliminiación del ticket
+        public DateTime? DeletedAt { get; set; }
+        //Responsable de la eliminación del ticket
+        public string? DeletedBy { get; set; }
+        //Fecha de creación del ticket
+        public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+        //Responsable de la craeción del ticket
+        public string? CreatedBy { get; set; }
+        //Fecha de edición del ticket
+        public DateTime? UpdateAt { get; set; }
+        //Responsable de la edición del ticket
+        public string? UpdateBy { get; set; }
+    }
+}
