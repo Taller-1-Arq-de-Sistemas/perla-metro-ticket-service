1181bc22125c4117b63f983007d994dd85424acd
Thetrolxs <moralesharnisch@gmail.com>
2025-09-21 17:00:12 -0300

    build: add automapper package

--- perla-metro-tickets-service/obj/Debug/net9.0/perla-metro-tickets-service.AssemblyInfo.cs
+++ perla-metro-tickets-service/obj/Debug/net9.0/perla-metro-tickets-service.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("perla-metro-tickets-service")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5e8a104ef1b5eac89fba53df61a2ca8a3551e73c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ab1a6bfd9ae9e413324aa607a89960ac7e2b4556")]
 [assembly: System.Reflection.AssemblyProductAttribute("perla-metro-tickets-service")]
 [assembly: System.Reflection.AssemblyTitleAttribute("perla-metro-tickets-service")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
