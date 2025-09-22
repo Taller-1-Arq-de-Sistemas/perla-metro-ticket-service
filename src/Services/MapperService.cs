259bcf9ff79a4ef7e343b62d5778a7f32fe3dd20
Thetrolxs <moralesharnisch@gmail.com>
2025-09-22 00:42:51 -0300

    feat: add mapper service

---
+++ src/Services/MapperService.cs
@@ -0,0 +1,45 @@
+using AutoMapper;
+using perla_metro_tickets_service.src.DTOs;
+using perla_metro_tickets_service.src.Interfaces;
+using perla_metro_tickets_service.src.models;
+
+namespace perla_metro_tickets_service.src.Services
+{
+    public class MapperService : IMapperService
+    {
+        private readonly IMapper _mapper;
+
+        public MapperService(IMapper mapper)
+        {
+            _mapper = mapper;
+        }
+
+        public Ticket CreateDtoToTicket(CreateTicketDto createTicketDto, string createdBy)
+        {
+            var ticket = _mapper.Map<Ticket>(createTicketDto);
+            ticket.CreatedBy = createdBy;
+            return ticket;
+        }
+
+        public IEnumerable<ViewTicketDto> TicketsToResponses(IEnumerable<Ticket> tickets, Func<Guid, string> passagerResolver)
+        {
+            return tickets.Select(ticket =>
+                TicketToResponse(ticket, passagerResolver(ticket.PassagerId))
+            ).ToList();
+        }
+
+        public ViewTicketDto TicketToResponse(Ticket ticket, string passagerName)
+        {
+            var response = _mapper.Map<ViewTicketDto>(ticket);
+            response.PassengerName = passagerName;
+            return response;
+        }
+
+        public void UpdateTicketFromDto(Ticket ticket, UpdateTicketDto updateTicketDto, string updateBy)
+        {
+            _mapper.Map(updateTicketDto, ticket);
+            ticket.UpdateBy = updateBy;
+            ticket.UpdateAt = DateTime.UtcNow;
+        }
+    }
+}
