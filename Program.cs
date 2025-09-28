1eb549314341bfe607af5d600c9e975f683cc679
Thetrolxs <moralesharnisch@gmail.com>
2025-09-28 15:50:25 -0300

    feat: add program modify based to api

--- Program.cs
+++ Program.cs
@@ -1,41 +1,41 @@
+using perla_metro_tickets_service.src.Data;
+using perla_metro_tickets_service.src.Helpers;
+using perla_metro_tickets_service.src.Interfaces;
+using perla_metro_tickets_service.src.Repositories;
+using perla_metro_tickets_service.src.Services;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
-builder.Services.AddOpenApi();
+//configuración de servicios a utilizar
+
+builder.Services.AddSingleton<MongoContext>();
+
+//Repositorios y servicios
+builder.Services.AddScoped<ITicketRepository, TicketRepository>();
+builder.Services.AddScoped<ITicketService, TicketService>();
+builder.Services.AddScoped<IMapperService, MapperService>();
+builder.Services.AddHttpClient<TicketService>(client =>
+{
+    client.BaseAddress = new Uri(builder.Configuration["MainApiBaseUrl"]!);
+});
+
+builder.Services.AddAutoMapper(cfg => { }, typeof(MappingProfile));
+
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-    app.MapOpenApi();
+    app.UseSwagger();
+    app.UseSwaggerUI();
 }
 
 app.UseHttpsRedirection();
-
-var summaries = new[]
-{
-    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-};
-
-app.MapGet("/weatherforecast", () =>
-{
-    var forecast =  Enumerable.Range(1, 5).Select(index =>
-        new WeatherForecast
-        (
-            DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-            Random.Shared.Next(-20, 55),
-            summaries[Random.Shared.Next(summaries.Length)]
-        ))
-        .ToArray();
-    return forecast;
-})
-.WithName("GetWeatherForecast");
+app.UseAuthentication();
+app.UseAuthorization(); 
+app.MapControllers();
 
 app.Run();
-
-record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
-{
-    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
-}
